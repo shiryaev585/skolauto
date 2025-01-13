19ba3da7a9ba080b41e09e4f019ee7878acc2da7
@shiryaev1707 <shiryaev1707@gmail.com>
2025-01-13 18:44:19 +0300

    in progress

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <span class="text">Такой страницы не существует</span>
+        <h1>{{ error.statusCode }}</h1>
+        <ui-btn @click="handleError">На главную</ui-btn>
+    </div>
+</template>
+
+<script setup>
+const error = useError();
+
+const handleError = () => {
+    clearError({
+        redirect: '/',
+    });
+};
+</script>
