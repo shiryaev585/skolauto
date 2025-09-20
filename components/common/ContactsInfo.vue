c8c58c5c94fb64ed90f085f3644cb98450a1abad
shiryaev585 <shiryaev1707@gmail.com>
2025-09-20 18:32:48 +0300

    in progress

--- components/common/ContactsInfo.vue
+++ components/common/ContactsInfo.vue
@@ -8,8 +8,7 @@
             <span class="text-xl w-28 text-gray-400">Адрес:</span>
             <a :href="addressHref" target="_blanc" class="text-xl link-hover">{{ contacts.address.value }}</a>
         </div>
-        <span class="text-xl">Заезд справа от&nbsp;мойки самообслуживания (территория бывшего АТП).</span>
-        <span class="text-xl">Ежедневно с&nbsp;08-00 до&nbsp;20-00, кроме Воскресения.</span>
+        <span class="text-xl">Заезд справа от&nbsp;мойки самообслуживания (территория бывшего АТП). Ежедневно с&nbsp;08-00 до&nbsp;20-00, кроме Воскресения.</span>
     </div>
 </template>
 
--- pages/prices.vue
+++ pages/prices.vue
@@ -3,38 +3,3 @@
         <common-prices class="section-top-offset" :prices="prices" />
     </div>
 </template>
-
-<script setup>
-const prices = [
-    {
-        id: 1,
-        label: 'Ремонт трещины',
-        value: 'от 2000 ₽'
-    },
-    {
-        id: 2,
-        label: 'Ремонт скола',
-        value: 'от 5000 ₽'
-    },
-    {
-        id: 3,
-        label: 'Ремонт лобового стекла',
-        value: 'от 2000 ₽'
-    },
-    {
-        id: 4,
-        label: 'Ремонт бокового',
-        value: 'от 5000 ₽'
-    },
-    {
-        id: 5,
-        label: 'Ремонт ещё чего-то',
-        value: 'от 2000 ₽'
-    },
-    {
-        id: 6,
-        label: 'Ремонт ещё чего-то',
-        value: 'от 5000 ₽'
-    }
-]
-</script>
